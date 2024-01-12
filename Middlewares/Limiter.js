5ccaa95839cb754b5a78741ac3457486e4f39b95
Abhishek Gupta <abhigpta19@gmail.com>
2024-01-13 00:34:55 +0530

    Set limit to 1000 request in 15 minutes

--- Middlewares/Limiter.js
+++ Middlewares/Limiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 //set the time in minutes and number of requests in that time need to be limited
 const time_in_minutes = 15;
-const max_limit_of_requests = 5;
+const max_limit_of_requests = 1000;
 
 
 //implemented rate limiter
